cc852c9ba2e7a7d68286beb614ca835fea4cf207
left <107749872+0xleft@users.noreply.github.com>
2025-01-19 19:58:38 +0100

    Update Translate.h

--- Translate.h
+++ Translate.h
@@ -3,6 +3,16 @@
 #include <vector>
 #include <string>
 #include "trnslt.h"
+#include <Windows.h>
+
+std::wstring utf8ToWstring(const std::string& str) {
+    if (str.empty()) return std::wstring();
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+    std::wstring wstrTo(size_needed, 0);
+    MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+    return wstrTo;
+}
+
 
 std::string wToString(wchar_t* string) {
     std::wstring ws(string);
