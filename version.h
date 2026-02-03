4f8a753d57ed1e33b0016747051b9ea97cec427d
ahmouse15 <as.abdulla@proton.me>
2026-02-03 09:51:22 -0600

    Update version number

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 3
-#define VERSION_BUILD 382
+#define VERSION_BUILD 386
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
